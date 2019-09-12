28494a3ce527ce49227e174af1f1050e67f8e69a
Quentin Dupont <perso@quentindupont.fr>
2019-09-12 15:03:38 +0200

    automatic migration

--- pos_journal_image/__manifest__.py
+++ pos_journal_image/__manifest__.py
@@ -1,11 +1,10 @@
-# coding: utf-8
 # Copyright (C) 2019 - Today: GRAP (http://www.grap.coop)
 # @author: Sylvain LE GAL (https://twitter.com/legalsylvain)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
     'name': "Point of Sale - Journal Image",
     'summary': "Add images on Account Journals available in the PoS",
-    'version': '8.0.1.0.0',
+    'version': '12.0.1.0.0',
     'category': 'Point of Sale',
     'author': 'GRAP',
     'website': 'http://www.grap.coop',
@@ -28,5 +27,5 @@
         'static/description/pos_payment_list_image.png',
         'static/description/pos_paypad_image.png',
     ],
-    'installable': False,
+    'installable': True,
 }
