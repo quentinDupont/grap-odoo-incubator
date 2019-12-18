05099eb575853e04764cd8916622a86fdaab4edc
Quentin Dupont <perso@quentindupont.fr>
2019-12-18 11:15:44 +0100

    1st commit : migration script + garde que ce qui nous concerne

---
+++ product_print_category_food_report/__manifest__.py
@@ -0,0 +1,38 @@
+# Copyright (C) 2018 - Today: GRAP (http://www.grap.coop)
+# @author: Sylvain LE GAL (https://twitter.com/legalsylvain)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "GRAP - Custom Qweb Reports",
+    "version": "12.0.1.0.0",
+    "category": "GRAP - Custom",
+    "author": "GRAP",
+    "website": "http://www.grap.coop",
+    "license": "AGPL-3",
+    "depends": [
+        "product_print_category",
+    ],
+    "data": [
+        "security/ir_rule.xml",
+        "security/ir.model.access.csv",
+        "report/qweb_template_product_product.xml",
+        "report/qweb_template_product_gen_label.xml",
+        "report/ir.actions.report.xml",
+        "views/menu.xml",
+        "views/view_res_company.xml",
+        "views/view_product_uom.xml",
+        "views/view_product_pricetag_type.xml",
+        "views/view_product_product.xml",
+        "views/view_product_template.xml",
+        "views/view_stock_picking.xml",
+        "data/product_print_category.xml",
+    ],
+    "demo": [
+        "demo/res_groups.xml",
+        "demo/product_uom.xml",
+        "demo/product_pricetag_type.xml",
+        "demo/product_product.xml",
+        "demo/product_print_category.xml",
+    ],
+    "installable": True,
+}
