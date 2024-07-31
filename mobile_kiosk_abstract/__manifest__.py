e876e2a7452866c372adbe13c5d64d4b8a65aa84
Quentin Dupont <quentin.dupont@grap.coop>
2024-07-31 15:16:56 +0200

    [MIG] mobile_kiosk_abstract: Migration to 16.0 (from 12.0)

--- mobile_kiosk_abstract/__manifest__.py
+++ mobile_kiosk_abstract/__manifest__.py
@@ -4,7 +4,7 @@
 
 {
     "name": "Mobile Kiosk Abstract",
-    "version": "12.0.1.1.6",
+    "version": "16.0.1.0.0",
     "author": "GRAP",
     "summary": "Abstract Module that provides a framework to develop"
     " 'kiosk application' for mobile usage like in 'hr_attendance'"
